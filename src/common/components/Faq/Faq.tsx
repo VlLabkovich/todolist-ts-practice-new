88b455c3a9da487ac4e13514bd63eb7a61fa6516
Vladislav <labkovich.vl@gmail.com>
2025-07-13 17:20:47 +0300

    refactor: change routing and faq

--- src/common/components/Faq/Faq.tsx
+++ src/common/components/Faq/Faq.tsx
@@ -1,14 +1,22 @@
 import Button from "@mui/material/Button"
 import { Link } from "react-router"
+import s from "./faq.module.css"
 
 export const Faq = () => {
   return (
-    <div style={{ textAlign: "center" }}>
+    <div className={s.contact}>
       <h1>Todolist IT-INCUBATOR</h1>
       <h2>Created by Frontend Developer - Labkovich Vladislav</h2>
-      <Button component={Link} to="/" variant="contained">
-        ВЕРНУТЬСЯ НА ГЛАВНУЮ
-      </Button>
+      <h3>How to contact me:</h3>
+      <div className={s.btnLinks}>
+        <Button component={Link} to="https://vllabkovich.github.io/my-portfolio-ts/">
+          My Portfolio
+        </Button>
+        <Button component={Link} to="/" variant="contained">
+          ВЕРНУТЬСЯ НА ГЛАВНУЮ
+        </Button>
+      </div>
+
     </div>
   )
 }
--- src/common/routing/Routing.tsx
+++ src/common/routing/Routing.tsx
@@ -21,13 +21,13 @@ export const Routing = () => {
     <Routes>
       <Route element={<ProtectedRoute isAllowed={isLoggedIn} />}>
         <Route path={Path.Main} element={<Main />} />
-        <Route path={Path.Faq} element={<Faq />} />
       </Route>
 
       <Route element={<ProtectedRoute isAllowed={!isLoggedIn} redirectPath={Path.Main} />}>
         <Route path={Path.Login} element={<Login />} />
       </Route>
       <Route path={Path.NotFound} element={<PageNotFound />} />
+      <Route path={Path.Faq} element={<Faq />} />
     </Routes>
   )
 }
