46792491a0bfb1c71a7681a12ecf8531439a247a
Vladislav <labkovich.vl@gmail.com>
2025-08-27 15:12:08 +0300

    feat: add pagination

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -1,15 +1,19 @@
 import { baseApi } from "@/app/baseApi.ts"
+import { PAGE_SIZE } from "@/common/constants"
 import type { BaseResponse } from "@/common/types"
 import type { DomainTask, GetTasksResponse, UpdateTaskModel } from "./tasksApi.types"
 
 export const tasksApi = baseApi.injectEndpoints({
   endpoints: (build) => ({
-    getTasks: build.query<GetTasksResponse, string>({
-      query: (todolistId) => ({
-        url: `/todo-lists/${todolistId}/tasks`,
-        method: "GET",
-      }),
-      providesTags: (_res, _err, todolistId) => [{ type: "Task", id: todolistId }],
+    getTasks: build.query<GetTasksResponse, { todolistId: string; params: { page: number } }>({
+      query: ({ todolistId, params }) => {
+        return {
+          url: `/todo-lists/${todolistId}/tasks/`,
+          params: { ...params, count: PAGE_SIZE },
+          method: "GET",
+        }
+      },
+      providesTags: (_res, _err, { todolistId }) => [{ type: "Task", id: todolistId }],
     }),
     createTask: build.mutation<BaseResponse<{ item: DomainTask }>, { todolistId: string; title: string }>({
       query: ({ todolistId, title }) => ({
--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/Tasks.tsx
@@ -1,8 +1,10 @@
 import { TaskStatus } from "@/common/enums"
 import { useGetTasksQuery } from "@/features/todolists/api/tasksApi.ts"
 import type { DomainTodolist } from "@/features/todolists/lib/types"
+import { TasksPagination } from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksPagination/TasksPagination.tsx"
 import { TasksSkeleton } from "@/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksSkeleton"
 import List from "@mui/material/List"
+import { useState } from "react"
 import { TaskItem } from "./TaskItem/TaskItem"
 
 type Props = {
@@ -12,10 +14,12 @@ type Props = {
 export const Tasks = ({ todolist }: Props) => {
   const { id, filter } = todolist
 
-  // const dispatch = useAppDispatch()
+  const [page, setPage] = useState(1)
 
-  const { data, isLoading } = useGetTasksQuery(id)
-
+  const { data, isLoading } = useGetTasksQuery({
+    todolistId: id,
+    params: { page },
+  })
   if (isLoading) {
     return <TasksSkeleton />
   }
@@ -34,10 +38,10 @@ export const Tasks = ({ todolist }: Props) => {
         <p>Тасок нет</p>
       ) : (
         <List>
-          {filteredTasks &&
-            filteredTasks.map((task) => <TaskItem key={task.id} task={task} todolistId={id} todolist={todolist} />)}
+          {filteredTasks?.map((task) => <TaskItem key={task.id} task={task} todolist={todolist} />)}
         </List>
       )}
+      <TasksPagination totalCount={data?.totalCount || 0} page={page} setPage={setPage} />
     </>
   )
 }
