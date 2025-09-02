f67672b8f7974eafe925470e3d87c2323830d7ec
Vladislav <labkovich.vl@gmail.com>
2025-09-02 22:08:12 +0300

    refactor: change pagination (homework)

--- src/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksPagination/TasksPagination.tsx
+++ src/features/todolists/ui/Todolists/TodolistItem/Tasks/TasksPagination/TasksPagination.tsx
@@ -1,8 +1,8 @@
-import { PAGE_SIZE } from '@/common/constants'
-import Pagination from '@mui/material/Pagination'
-import Typography from '@mui/material/Typography'
-import { ChangeEvent } from 'react'
-import styles from './TasksPagination.module.css'
+import { PAGE_SIZE } from "@/common/constants"
+import Pagination from "@mui/material/Pagination"
+import Typography from "@mui/material/Typography"
+import { ChangeEvent, useEffect } from "react"
+import styles from "./TasksPagination.module.css"
 
 type Props = {
   totalCount: number
@@ -14,16 +14,25 @@ export const TasksPagination = ({ totalCount, page, setPage }: Props) => {
   const changePage = (_: ChangeEvent<unknown>, page: number) => {
     setPage(page)
   }
+
+  useEffect(() => {
+    if(totalCount === PAGE_SIZE) {
+      setPage(1)
+    }
+  }, [totalCount])
+
   return (
     <>
-      <Pagination
-        count={Math.ceil(totalCount / PAGE_SIZE)}
-        page={page}
-        onChange={changePage}
-        shape="rounded"
-        color="primary"
-        className={styles.pagination}
-      />
+      {totalCount > PAGE_SIZE && (
+        <Pagination
+          count={Math.ceil(totalCount / PAGE_SIZE)}
+          page={page}
+          onChange={changePage}
+          shape="rounded"
+          color="primary"
+          className={styles.pagination}
+        />
+      )}
       <div className={styles.totalCount}>
         <Typography variant="caption">Total: {totalCount}</Typography>
       </div>
